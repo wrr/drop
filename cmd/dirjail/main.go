067054336a32f757cc3d50dc0ff1c63cb43de0b8
Jan Wrobel <jan@mixedbit.org>
2025-07-23 11:39:15 +0200

    An internal command line flag to allow to exec a jailed child proccess

--- cmd/dirjail/main.go
+++ cmd/dirjail/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func childProcessEntry() {
+}
+
+func die(err error) {
+	fmt.Fprintln(os.Stderr, "Error:", err)
+	os.Exit(1)
+
+}
+
 func main() {
+
+	if len(os.Args) > 1 && os.Args[1] == "-child" {
+		fmt.Println("Child started")
+		childProcessEntry()
+		os.Exit(0)
+	}
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "dirjail limits programs abilities to read and write user's files\nOptions:\n")
+		flag.PrintDefaults()
+	}
+
+	err := flag.CommandLine.Parse(os.Args[1:])
+	if err != nil {
+		die(err)
+	}
 
 }
